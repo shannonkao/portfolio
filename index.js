a6e5d9fcf6daad8d3782ea3e19525ce6c66c6f26
shannon <skao127@gmail.com>
2018-03-24 00:11:30 -0700

    Initial waterfall structure and images

---
+++ index.js
@@ -0,0 +1,35 @@
+var _image_path = "img/";
+var _image_list = ["vast.png", "roadtrip-bison.png", 
+                   "fatt-mystery.png", "fatt-sun.png",
+                   "forged.png", "doorway.png",
+                   "city-1.png", "city-2.png"];
+
+var _col_count = 2;
+
+$(document).ready(function() {
+    $('#header-icons').hide();
+    
+    var waterfall = $("#waterfall");
+    
+    for (var i = 0; i < _col_count; i++) {
+        var col = waterfall.append($('<div>', {class: "col"}));
+    }
+
+    for (var i = 0, c = 0; i < _image_list.length; i++, c++) {
+        c = c % _col_count;
+        var img_cont = $('<div>', {class: "img-cont"});
+        img_cont.append($('<img>', {src: _image_path + _image_list[i]}));
+        $(".col").eq(c).append(img_cont);
+    }
+    
+    $('#header-contact').on({
+        'mouseenter': function() {
+            console.log("mouseenter")
+            $('#header-contact-link').fadeTo('normal', 0);
+            $('#header-icons').fadeTo('normal', 1);
+        },'mouseleave': function() {
+            $('#header-contact-link').fadeTo('normal', 1);
+            $('#header-icons').fadeTo('normal', 0);
+        }
+    });
+});
