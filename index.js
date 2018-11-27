b122016456693abeecf9499691bfd644dfcebddf
shannon <skao127@gmail.com>
2018-11-26 20:40:09 -0800

    Three column layout, update images

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
 var _image_path = "img/";
-var _image_list = ["bottle.gif", "deer.png", 
-                   "vast.png", "roadtrip-bison.png", 
-                   "fatt-mystery.png", "fatt-sun.png",
-                   "doorway.png", "roadtrip-baishe.png", 
-                   "bulb-1.png", "forged.png",
-                   "tea.png", "bulb-2.png",
-                   "motel.png", "lake.png",
-                   "city-1.png", "city-2.png"];
-
-var _col_count = 2;
+var _image_list = ["bus-1.png", "roadtrip-bison.png", "deer.png",
+                   "bus-2.png", "vast.png", "fatt-mystery.png",
+                   "doorway.png", "motel.png", "fatt-sun.png",
+                   "roadtrip-golem.png", "roadtrip-baishe.png", "roadtrip-jackal.png",
+                   "lake.png", "city-1.png", "city-2.png"];
+var _col_count;
 
 $(document).ready(function() {
-    var waterfall = $("#waterfall");
-    
-    for (var i = 0; i < _col_count; i++) {
-        var col = waterfall.append($('<div>', {class: "col"}));
-    }
-
-    for (var i = 0, c = 0; i < _image_list.length; i++, c++) {
-        c = c % _col_count;
-        if (_image_list[i] != "") {
-            var img_cont = $('<div>', {class: "img-cont"});
-            img_cont.append($('<img>', {src: _image_path + _image_list[i]}));
-            $(".col").eq(c).append(img_cont);
-        }
-    }
+   layout_images();
+   window.addEventListener('resize', layout_images);
     
     $('#header-contact').on({
         'mouseenter': function() {
-            console.log("mouseenter")
             $('#header-contact-link').addClass('hide');
             $('#header-icons').removeClass('hide');
         },'mouseleave': function() {
@@ -37,3 +20,31 @@ $(document).ready(function() {
         }
     });
 });
+
+var col_count = function() {
+    return window.innerWidth < 900 ? 2 : 3;
+}
+
+var layout_images = function() {
+    var new_count = col_count();
+
+    if (new_count != _col_count) {
+        _col_count = new_count;
+
+        var waterfall = $("#waterfall");
+        waterfall.empty();
+
+        for (var i = 0; i < _col_count; i++) {
+            var col = waterfall.append($('<div>', {class: "col"}));
+        }
+
+        for (var i = 0, c = 0; i < _image_list.length; i++, c++) {
+            c = c % _col_count;
+            if (_image_list[i] != "") {
+                var img_cont = $('<div>', {class: "img-cont"});
+                img_cont.append($('<img>', {src: _image_path + _image_list[i]}));
+                $(".col").eq(c).append(img_cont);
+            }
+        }
+    }
+}
