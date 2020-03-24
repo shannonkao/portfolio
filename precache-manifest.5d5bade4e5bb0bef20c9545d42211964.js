ce499c421b71ea747e0d05e1943271d12cec6a3d
unknown <skao127@gmail.com>
2020-03-24 08:48:39 -0700

    Updates

---
+++ precache-manifest.5d5bade4e5bb0bef20c9545d42211964.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f75cbb1d18caa3c7c51d254e8b18a5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c16918b7b343c4d0d47f",
+    "url": "/portfolio/static/css/main.62e1161f.chunk.css"
+  },
+  {
+    "revision": "07df45bf612478faab6e",
+    "url": "/portfolio/static/js/2.1c251a10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1c251a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16918b7b343c4d0d47f",
+    "url": "/portfolio/static/js/main.92ec0103.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "3f9cc15f0cd0d691161aa75ba67f0610",
+    "url": "/portfolio/static/media/b_fish.3f9cc15f.png"
+  },
+  {
+    "revision": "aec0711dc7b148e1688f071efd828f9d",
+    "url": "/portfolio/static/media/b_swamp.aec0711d.png"
+  },
+  {
+    "revision": "3f7a7b58d31424e951e86d1f712def22",
+    "url": "/portfolio/static/media/bottle.3f7a7b58.gif"
+  },
+  {
+    "revision": "75b46a2a74d2db0c50eb6615d533fd3e",
+    "url": "/portfolio/static/media/bus-1.75b46a2a.png"
+  },
+  {
+    "revision": "507dc425b449a15b52d5805269472808",
+    "url": "/portfolio/static/media/bus-2.507dc425.png"
+  },
+  {
+    "revision": "0fdcf2b3e15e89a2fb103823f043049d",
+    "url": "/portfolio/static/media/caves-1.0fdcf2b3.png"
+  },
+  {
+    "revision": "443dbb676de5731957fc550edaeb66c3",
+    "url": "/portfolio/static/media/caves-2.443dbb67.png"
+  },
+  {
+    "revision": "62d8e942506b00eee025296ac33fe2ac",
+    "url": "/portfolio/static/media/city-1.62d8e942.png"
+  },
+  {
+    "revision": "b868cbe72af9b5334ea553e954762550",
+    "url": "/portfolio/static/media/city-2.b868cbe7.png"
+  },
+  {
+    "revision": "2b1bf37cd575b6a5ef87bd9c9fcdfb94",
+    "url": "/portfolio/static/media/date-1.2b1bf37c.png"
+  },
+  {
+    "revision": "822b790f29bb645c465c314fee58220d",
+    "url": "/portfolio/static/media/date-2.822b790f.png"
+  },
+  {
+    "revision": "7544bc69e21359d0709be90ae1dc073d",
+    "url": "/portfolio/static/media/date-cover.7544bc69.png"
+  },
+  {
+    "revision": "5afae53236bd783640cca1fb3389f449",
+    "url": "/portfolio/static/media/deer.5afae532.png"
+  },
+  {
+    "revision": "dfbcc7f2bb499fde6ae15dd91d0bfb61",
+    "url": "/portfolio/static/media/discworld-tiffany.dfbcc7f2.png"
+  },
+  {
+    "revision": "5c1d0cc614e0e0780c00e02e0ecad97c",
+    "url": "/portfolio/static/media/door.5c1d0cc6.png"
+  },
+  {
+    "revision": "84323be7d99ba99cbd112163c516cb30",
+    "url": "/portfolio/static/media/doorway.84323be7.png"
+  },
+  {
+    "revision": "f7db801d962612dec9beb06f10031483",
+    "url": "/portfolio/static/media/fatt-icon-1.f7db801d.jpg"
+  },
+  {
+    "revision": "6cdc93370e0473e9fd94b39fabf108d0",
+    "url": "/portfolio/static/media/fatt-icon-2.6cdc9337.jpg"
+  },
+  {
+    "revision": "5fced589dbe63d7a4a87c54c9121519c",
+    "url": "/portfolio/static/media/fatt-mystery.5fced589.png"
+  },
+  {
+    "revision": "82f8d89f3ce8ba67975c9c96fd279cce",
+    "url": "/portfolio/static/media/fatt-sun.82f8d89f.png"
+  },
+  {
+    "revision": "ebde0b2f0da81f93d612eb4aa417b1ff",
+    "url": "/portfolio/static/media/fires-1.ebde0b2f.png"
+  },
+  {
+    "revision": "d17bcb6a6fb89ddd85132b9a7c2e51f9",
+    "url": "/portfolio/static/media/fires-2.d17bcb6a.png"
+  },
+  {
+    "revision": "fe8fde9c1aaa9e930d08bd20e6ff62d2",
+    "url": "/portfolio/static/media/forged.fe8fde9c.png"
+  },
+  {
+    "revision": "6ef0bdd9ff42d4c39c03817711866a33",
+    "url": "/portfolio/static/media/garden.6ef0bdd9.png"
+  },
+  {
+    "revision": "87b24e6b173af3eca8d4fe91e9985812",
+    "url": "/portfolio/static/media/krz.87b24e6b.png"
+  },
+  {
+    "revision": "a426f009e499dd76a17ec983adc016ff",
+    "url": "/portfolio/static/media/l_jellies.a426f009.jpg"
+  },
+  {
+    "revision": "e2d1b8cd1dfd0b0c277d78023d17b1d4",
+    "url": "/portfolio/static/media/l_plants.e2d1b8cd.jpg"
+  },
+  {
+    "revision": "010b27ce1e46a2109ae25c498d3e6f6a",
+    "url": "/portfolio/static/media/lake.010b27ce.png"
+  },
+  {
+    "revision": "9381090684a77e442746129351a53ef5",
+    "url": "/portfolio/static/media/mail-1.93810906.png"
+  },
+  {
+    "revision": "4464e9f150b893f60410c2cff505a31f",
+    "url": "/portfolio/static/media/mail-2.4464e9f1.png"
+  },
+  {
+    "revision": "3cf6ec6fd793ca478929c3226ae8fbef",
+    "url": "/portfolio/static/media/motel.3cf6ec6f.png"
+  },
+  {
+    "revision": "b0ad1036f65a4795553c8d04c714b11a",
+    "url": "/portfolio/static/media/phone-1.b0ad1036.png"
+  },
+  {
+    "revision": "0edc6a16f0184c8e5dd2edd130f6e20d",
+    "url": "/portfolio/static/media/phone-2.0edc6a16.png"
+  },
+  {
+    "revision": "2defd9b0675d3e79ac0221aa26c789cb",
+    "url": "/portfolio/static/media/remains-1.2defd9b0.png"
+  },
+  {
+    "revision": "484bd7c40aa0ceb6d102361955315523",
+    "url": "/portfolio/static/media/remains-2.484bd7c4.png"
+  },
+  {
+    "revision": "f2f7b16b2b53cc06883a641f9d829d3c",
+    "url": "/portfolio/static/media/rgu-stairs.f2f7b16b.jpg"
+  },
+  {
+    "revision": "ec432de3ceff35ee07780c3ec6ad056f",
+    "url": "/portfolio/static/media/roadtrip-baishe.ec432de3.png"
+  },
+  {
+    "revision": "d6a40c4f71cf31417381aae5ac0410f5",
+    "url": "/portfolio/static/media/roadtrip-bison.d6a40c4f.png"
+  },
+  {
+    "revision": "7547a9333028694919bbb3fecfeec47e",
+    "url": "/portfolio/static/media/roadtrip-golem.7547a933.png"
+  },
+  {
+    "revision": "3572f830c9f98b8c3fab5b81edbe2d11",
+    "url": "/portfolio/static/media/roadtrip-jackal.3572f830.png"
+  },
+  {
+    "revision": "a20be1204a213bee7dab96d39f5e15e9",
+    "url": "/portfolio/static/media/smoke.a20be120.png"
+  },
+  {
+    "revision": "ef349be2584507dce526ade5ad10cdc8",
+    "url": "/portfolio/static/media/stars.ef349be2.png"
+  },
+  {
+    "revision": "ed78646784e7c267d71eb17b6f9078fa",
+    "url": "/portfolio/static/media/summer-1.ed786467.png"
+  },
+  {
+    "revision": "655a60de78147aa990ef950da43f6305",
+    "url": "/portfolio/static/media/summer-2.655a60de.png"
+  },
+  {
+    "revision": "e968bcb258621d083c96d49a941b0519",
+    "url": "/portfolio/static/media/tumblr.e968bcb2.svg"
+  },
+  {
+    "revision": "835a4e85c7b3c70fc95c8186e33c0b12",
+    "url": "/portfolio/static/media/twitter.835a4e85.svg"
+  },
+  {
+    "revision": "fdbff9ee56ddb3c442b77f7b915c4999",
+    "url": "/portfolio/static/media/v_sky.fdbff9ee.jpg"
+  },
+  {
+    "revision": "5d477f5ea32d5053f8dcdf5a2316449c",
+    "url": "/portfolio/static/media/v_stars.5d477f5e.jpg"
+  },
+  {
+    "revision": "c40f6512da74a8a740e109ece1fa2404",
+    "url": "/portfolio/static/media/vast.c40f6512.png"
+  },
+  {
+    "revision": "cf49262b358832e6f1007d9c91b61b3b",
+    "url": "/portfolio/static/media/w_fish.cf49262b.png"
+  },
+  {
+    "revision": "d85ac972e1df384c857e032d49313e1d",
+    "url": "/portfolio/static/media/w_moon.d85ac972.png"
+  },
+  {
+    "revision": "caf59260093f4d5964a74a8aa84d8068",
+    "url": "/portfolio/static/media/w_trees.caf59260.png"
+  },
+  {
+    "revision": "f0057357c0458dcf566e204c16a45921",
+    "url": "/portfolio/static/media/winter.f0057357.png"
+  },
+  {
+    "revision": "b86333a35d361ec9a35bd3a2ed476b10",
+    "url": "/portfolio/static/media/yc-cover.b86333a3.png"
+  }
+]);
