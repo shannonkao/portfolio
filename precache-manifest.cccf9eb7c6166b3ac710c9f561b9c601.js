76b2a9db693e58b9f081be27613919089d8842c2
unknown <skao127@gmail.com>
2020-03-24 11:05:51 -0700

    Restore files

---
+++ precache-manifest.cccf9eb7c6166b3ac710c9f561b9c601.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2017dfe5f61a7ed21bd8d4c9b6cf1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "725eb39778c7bfc4051f",
+    "url": "/static/css/main.3cf78aa8.chunk.css"
+  },
+  {
+    "revision": "aa342a60d6b679b71550",
+    "url": "/static/js/2.77654dd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77654dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "725eb39778c7bfc4051f",
+    "url": "/static/js/main.eeafe9a0.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "3f9cc15f0cd0d691161aa75ba67f0610",
+    "url": "/static/media/b_fish.3f9cc15f.png"
+  },
+  {
+    "revision": "aec0711dc7b148e1688f071efd828f9d",
+    "url": "/static/media/b_swamp.aec0711d.png"
+  },
+  {
+    "revision": "3f7a7b58d31424e951e86d1f712def22",
+    "url": "/static/media/bottle.3f7a7b58.gif"
+  },
+  {
+    "revision": "75b46a2a74d2db0c50eb6615d533fd3e",
+    "url": "/static/media/bus-1.75b46a2a.png"
+  },
+  {
+    "revision": "507dc425b449a15b52d5805269472808",
+    "url": "/static/media/bus-2.507dc425.png"
+  },
+  {
+    "revision": "62d8e942506b00eee025296ac33fe2ac",
+    "url": "/static/media/city-1.62d8e942.png"
+  },
+  {
+    "revision": "b868cbe72af9b5334ea553e954762550",
+    "url": "/static/media/city-2.b868cbe7.png"
+  },
+  {
+    "revision": "5afae53236bd783640cca1fb3389f449",
+    "url": "/static/media/deer.5afae532.png"
+  },
+  {
+    "revision": "dfbcc7f2bb499fde6ae15dd91d0bfb61",
+    "url": "/static/media/discworld-tiffany.dfbcc7f2.png"
+  },
+  {
+    "revision": "5c1d0cc614e0e0780c00e02e0ecad97c",
+    "url": "/static/media/door.5c1d0cc6.png"
+  },
+  {
+    "revision": "84323be7d99ba99cbd112163c516cb30",
+    "url": "/static/media/doorway.84323be7.png"
+  },
+  {
+    "revision": "f7db801d962612dec9beb06f10031483",
+    "url": "/static/media/fatt-icon-1.f7db801d.jpg"
+  },
+  {
+    "revision": "6cdc93370e0473e9fd94b39fabf108d0",
+    "url": "/static/media/fatt-icon-2.6cdc9337.jpg"
+  },
+  {
+    "revision": "5fced589dbe63d7a4a87c54c9121519c",
+    "url": "/static/media/fatt-mystery.5fced589.png"
+  },
+  {
+    "revision": "82f8d89f3ce8ba67975c9c96fd279cce",
+    "url": "/static/media/fatt-sun.82f8d89f.png"
+  },
+  {
+    "revision": "fe8fde9c1aaa9e930d08bd20e6ff62d2",
+    "url": "/static/media/forged.fe8fde9c.png"
+  },
+  {
+    "revision": "6ef0bdd9ff42d4c39c03817711866a33",
+    "url": "/static/media/garden.6ef0bdd9.png"
+  },
+  {
+    "revision": "87b24e6b173af3eca8d4fe91e9985812",
+    "url": "/static/media/krz.87b24e6b.png"
+  },
+  {
+    "revision": "a426f009e499dd76a17ec983adc016ff",
+    "url": "/static/media/l_jellies.a426f009.jpg"
+  },
+  {
+    "revision": "e2d1b8cd1dfd0b0c277d78023d17b1d4",
+    "url": "/static/media/l_plants.e2d1b8cd.jpg"
+  },
+  {
+    "revision": "010b27ce1e46a2109ae25c498d3e6f6a",
+    "url": "/static/media/lake.010b27ce.png"
+  },
+  {
+    "revision": "9381090684a77e442746129351a53ef5",
+    "url": "/static/media/mail-1.93810906.png"
+  },
+  {
+    "revision": "4464e9f150b893f60410c2cff505a31f",
+    "url": "/static/media/mail-2.4464e9f1.png"
+  },
+  {
+    "revision": "3cf6ec6fd793ca478929c3226ae8fbef",
+    "url": "/static/media/motel.3cf6ec6f.png"
+  },
+  {
+    "revision": "b0ad1036f65a4795553c8d04c714b11a",
+    "url": "/static/media/phone-1.b0ad1036.png"
+  },
+  {
+    "revision": "0edc6a16f0184c8e5dd2edd130f6e20d",
+    "url": "/static/media/phone-2.0edc6a16.png"
+  },
+  {
+    "revision": "f2f7b16b2b53cc06883a641f9d829d3c",
+    "url": "/static/media/rgu-stairs.f2f7b16b.jpg"
+  },
+  {
+    "revision": "ec432de3ceff35ee07780c3ec6ad056f",
+    "url": "/static/media/roadtrip-baishe.ec432de3.png"
+  },
+  {
+    "revision": "d6a40c4f71cf31417381aae5ac0410f5",
+    "url": "/static/media/roadtrip-bison.d6a40c4f.png"
+  },
+  {
+    "revision": "7547a9333028694919bbb3fecfeec47e",
+    "url": "/static/media/roadtrip-golem.7547a933.png"
+  },
+  {
+    "revision": "3572f830c9f98b8c3fab5b81edbe2d11",
+    "url": "/static/media/roadtrip-jackal.3572f830.png"
+  },
+  {
+    "revision": "ef349be2584507dce526ade5ad10cdc8",
+    "url": "/static/media/stars.ef349be2.png"
+  },
+  {
+    "revision": "e968bcb258621d083c96d49a941b0519",
+    "url": "/static/media/tumblr.e968bcb2.svg"
+  },
+  {
+    "revision": "835a4e85c7b3c70fc95c8186e33c0b12",
+    "url": "/static/media/twitter.835a4e85.svg"
+  },
+  {
+    "revision": "fdbff9ee56ddb3c442b77f7b915c4999",
+    "url": "/static/media/v_sky.fdbff9ee.jpg"
+  },
+  {
+    "revision": "5d477f5ea32d5053f8dcdf5a2316449c",
+    "url": "/static/media/v_stars.5d477f5e.jpg"
+  },
+  {
+    "revision": "c40f6512da74a8a740e109ece1fa2404",
+    "url": "/static/media/vast.c40f6512.png"
+  },
+  {
+    "revision": "cf49262b358832e6f1007d9c91b61b3b",
+    "url": "/static/media/w_fish.cf49262b.png"
+  },
+  {
+    "revision": "d85ac972e1df384c857e032d49313e1d",
+    "url": "/static/media/w_moon.d85ac972.png"
+  },
+  {
+    "revision": "caf59260093f4d5964a74a8aa84d8068",
+    "url": "/static/media/w_trees.caf59260.png"
+  },
+  {
+    "revision": "b86333a35d361ec9a35bd3a2ed476b10",
+    "url": "/static/media/yc-cover.b86333a3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5d5bade4e5bb0bef20c9545d42211964.js"
+  "/precache-manifest.cccf9eb7c6166b3ac710c9f561b9c601.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
