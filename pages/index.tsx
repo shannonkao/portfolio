f28a946434bc37194dcaaa44468bc7ba9cd5c43b
unknown <1120911+shannonkao@users.noreply.github.com>
2024-10-07 17:06:30 -0700

    Basic mobile cleanup of landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,12 +12,6 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <main className={styles.main}>
         <Header />
-        <div className={styles.gc}>
-          {projects.map(p => {
-              return <Gallery id={p.id} key={p.id} paths={p.paths} prefix="projects/" name={p.name} year={p.year} medium={p.medium} description={p.description} priority={p.priority} />
-            })
-          }
-        </div>
         <div className={styles.toc}>
           <div className={styles.name}>shannon kao</div>
           <div>Shannon is a Seattle-based artist interested in exploring the intersection of natural and built environments through multidisciplinary practice, with a focus on book-making, print, and digital interfaces. They co-operate the small risograph press <a href="https://snackbreakstudio.net/" target="_blank" rel="noreferrer">snack break studio.</a></div>
@@ -27,6 +21,12 @@ const Home: NextPage = () => {
               })
             }
           </ul>
+        </div>
+        <div className={styles.gc}>
+          {projects.map(p => {
+              return <Gallery id={p.id} key={p.id} paths={p.paths} prefix="projects/" name={p.name} year={p.year} medium={p.medium} description={p.description} priority={p.priority} />
+            })
+          }
         </div>
       </main>
     </div>
